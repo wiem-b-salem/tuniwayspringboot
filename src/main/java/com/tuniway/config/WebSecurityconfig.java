81e249a089b172c3db0d946ae83a8a1500e34bc3
Baga6312 <oussema.benayech@gmail.com>
2025-12-14 20:57:23 +0100

    awos

--- src/main/java/com/tuniway/config/WebSecurityconfig.java
+++ src/main/java/com/tuniway/config/WebSecurityconfig.java
@@ -17,6 +17,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
+
+
+
+
 @Configuration
 @EnableMethodSecurity
 public class WebSecurityConfig {
